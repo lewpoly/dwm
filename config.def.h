796fb650f916cacae2bf8ed895611d1def31ad57
lewpoly <ldixon.design@gmail.com>
2021-08-06 18:53:43 -0400

    swallowed

--- config.def.h
+++ config.def.h
@@ -12,6 +12,7 @@ static const unsigned int gappiv        = 23;       /* vert inner gap between wi
 static const unsigned int gappoh        = 23;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov        = 23;       /* vert outer gap between windows and screen edge */
 static       int smartgaps              = 0;        /* 1 means no outer gap when there is only one window */
+static const int swallowfloating        = 0;        /* 1 means swallow floating windows by default */
 static const int showbar                = 1;        /* 0 means no bar */
 static const int topbar                 = 1;        /* 0 means bottom bar */
 static const char *fonts[]              = { "Iosevka Extended:size=11", "FuraCode Nerd Font:size=11" };
@@ -44,9 +45,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class           instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",          NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Alacritty",     NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,            NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
