43dddb705384eb559458cbd6fd1a84e0b1b1dad6
lewpoly <ldixon.design@gmail.com>
2021-08-14 12:33:25 -0400

    nothing

--- config.h
+++ config.h
@@ -49,6 +49,7 @@ static const Rule rules[] = {
 	 */
 	/* class           instance  title           tags mask   iscentered  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",          NULL,     NULL,           0,          1,          1,          0,           0,        -1 },
+	{ "Ranger",        NULL,     NULL,           0,          1,          1,          1,           0,        -1 },
 	{ "Alacritty",     NULL,     NULL,           0,          0,          0,          1,           0,        -1 },
 	{ NULL,            NULL,     "Event Tester", 0,          0,          0,          0,           1,        -1 }, /* xev */
 };
