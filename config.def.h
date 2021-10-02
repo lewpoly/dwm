28bc42a5c509ad6231a26655874e12d1034d8741
lewpoly <ldixon.design@gmail.com>
2021-10-02 12:49:21 -0400

    ungruv before pad

--- config.def.h
+++ config.def.h
@@ -7,10 +7,10 @@
 /* appearance */
 static const unsigned int borderpx      = 2;        /* border pixel of windows */
 static const unsigned int snap          = 32;       /* snap pixel */
-static const unsigned int gappih        = 23;       /* horiz inner gap between windows */
-static const unsigned int gappiv        = 23;       /* vert inner gap between windows */
-static const unsigned int gappoh        = 23;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov        = 23;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih        = 22;       /* horiz inner gap between windows */
+static const unsigned int gappiv        = 22;       /* vert inner gap between windows */
+static const unsigned int gappoh        = 22;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov        = 22;       /* vert outer gap between windows and screen edge */
 static       int smartgaps              = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating        = 0;        /* 1 means swallow floating windows by default */
 static const int showbar                = 1;        /* 0 means no bar */
@@ -21,17 +21,17 @@ static const char *brdowncmd[]          = { "brightnessctl", "set", "10%-", NULL
 static const char *upvol[]              = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%",     NULL };
 static const char *downvol[]            = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%",     NULL };
 static const char *mutevol[]            = { "/usr/bin/pactl", "set-sink-mute",   "@DEFAULT_SINK@", "toggle",  NULL };
-static const char col_gray1[]           = "#282828";
+static const char col_gray1[]           = "#232731";
 static const char col_gray2[]           = "#a89984";
 static const char col_gray3[]           = "#ebdbb2";
-static const char col_gray4[]           = "#fabd2f";
-static const char col_cyan[]            = "#d3869b";
-static const unsigned int baralpha     = 190;
+static const char col_gray4[]           = "#ffcc66";
+static const char col_cyan[]            = "#a3be8c";
+static const unsigned int baralpha     = 230;
 static const unsigned int borderalpha  = OPAQUE;
 static const char *colors[][3]          = {
  	/*               fg         bg         border   */
  	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
- 	[SchemeSel]  = { col_gray4, col_gray1,  col_gray4  },
+ 	[SchemeSel]  = { col_cyan, col_gray1,  col_gray4  },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
