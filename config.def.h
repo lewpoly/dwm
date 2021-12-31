f5a010d910169cff1186d40204a0259a02f2114a
lewpoly <ldixon.design@gmail.com>
2021-12-30 20:36:28 -0500

    removed alpha, added systray

--- config.def.h
+++ config.def.h
@@ -7,6 +7,11 @@
 /* appearance */
 static const unsigned int borderpx      = 1;        /* border pixel of windows */
 static const unsigned int snap          = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray            = 1;     /* 0 means no systray */
 static const unsigned int gappih        = 23;       /* horiz inner gap between windows */
 static const unsigned int gappiv        = 23;       /* vert inner gap between windows */
 static const unsigned int gappoh        = 23;       /* horiz outer gap between windows and screen edge */
@@ -26,17 +31,10 @@ static const char col_gray2[]           = "#D8DEE9";
 static const char col_gray3[]           = "#bbbbbb";
 static const char col_gray4[]           = "#eeeeee";
 static const char col_cyan[]            = "#36b397";
-static const unsigned int baralpha     = 0xd0;
-static const unsigned int borderalpha  = OPAQUE;
 static const char *colors[][3]          = {
  	/*               fg         bg         border   */
  	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
  	[SchemeSel]  = { col_cyan, col_gray1,  col_cyan  },
-};
-static const unsigned int alphas[][3]      = {
-	/*               fg      bg        border     */
-  [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
@@ -101,11 +99,12 @@ static Key keys[] = {
 	/* modifier                     key                        function        argument */
 	{ MODKEY,                       XK_d,                      spawn,          SHCMD("dmenu_run -c -l 15") },
 	{ MODKEY,                       XK_BackSpace,              spawn,          SHCMD("sysact") },
-	{ MODKEY,			                  XK_p,                      spawn,		       SHCMD("feh --bg-fill --randomize ~/pics/wallpaper/* &") },
+	{ MODKEY,			                  XK_semicolon,                      spawn,		       SHCMD("feh --bg-fill --randomize ~/pics/wallpaper/* &") },
 	{ MODKEY,			                  XK_r,                      spawn,		       SHCMD(TERMINAL " -e ranger") },
 	{ MODKEY,			                  XK_y,                      spawn,		       SHCMD("flameshot gui -p ~/pics/screenshots") },
 	{ MODKEY|ShiftMask,			        XK_y,                      spawn,		       SHCMD("flameshot full -p ~/pics/screenshots") },
 	{ MODKEY|ShiftMask,			        XK_v,                      spawn,		       SHCMD("mpv /dev/video0") },
+  { MODKEY,                       XK_a,                      spawn,          SHCMD("skippy-xd") },
 	{ MODKEY,			                  XK_w,                      spawn,		       {.v = webcmd } },
 	{ MODKEY,                       XK_Return,                 spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_b,                      togglebar,      {0} },
@@ -189,4 +188,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
